135f4b0daf6f8ac25de2024704372f938e3f2362
ysr_feng <1282855229@qq.com>
2017-06-27 18:57:53 +0800

    初始化

---
+++ app/src/main/java/com/ysr/express/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ysr.express
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
