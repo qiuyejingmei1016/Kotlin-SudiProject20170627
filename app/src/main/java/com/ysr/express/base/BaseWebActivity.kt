411c7b7f1a09865a871afadd350f38bd4de891e3
ysrfeng <ysr200808@163.com>
2017-07-13 09:03:20 +0800

    基类webActivity

---
+++ app/src/main/java/com/ysr/express/base/BaseWebActivity.kt
@@ -0,0 +1,19 @@
+package com.ysr.express.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * 基类WebActivity
+ * Created by ysr on 2017/7/13 8:48.
+ * 邮箱 ysr200808@163.com
+ */
+abstract class BaseWebActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId)
+    }
+
+    abstract val getLayoutId: Int
+}
