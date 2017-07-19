b6b994946a1e276f144ea230816f60f6be7e680b
ysrfeng <ysr200808@163.com>
2017-07-19 08:26:35 +0800

    物流详情返回事件

--- app/src/main/java/com/ysr/express/ui/activity/SearchDetailsActivity.kt
+++ app/src/main/java/com/ysr/express/ui/activity/SearchDetailsActivity.kt
@@ -1,7 +1,7 @@
 package com.ysr.express.ui.activity
 
 import android.support.v7.widget.LinearLayoutManager
-import android.view.View
+import android.view.MenuItem
 import com.ysr.express.R
 import com.ysr.express.adapter.TraceListAdapter
 import com.ysr.express.bean.RequestEbsDetail
@@ -16,12 +16,11 @@ import retrofit2.Response
 import java.util.*
 
 
-
 /**订单轨迹
  * Created by ysr on 2017/7/14 14:09.
  * 邮箱 ysr200808@163.com
  */
-class SearchDetailsActivity : BaseActivity(), View.OnClickListener {
+class SearchDetailsActivity : BaseActivity() {
     private var traceList: List<RequestEbsDetail.TracesBean> = ArrayList()
     private var adapter: TraceListAdapter? = null
 
@@ -34,13 +33,21 @@ class SearchDetailsActivity : BaseActivity(), View.OnClickListener {
     }
 
     override fun initView() {
+        setSupportActionBar(toolbar_DetailsActivity)
+        supportActionBar?.setTitle(R.string.text_null)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         adapter = TraceListAdapter(this, traceList)
         rvTrace.layoutManager = LinearLayoutManager(this)
         rvTrace.adapter = adapter
         loadData()
     }
 
-    override fun onClick(v: View) {
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
 
     }
 
@@ -52,15 +59,15 @@ class SearchDetailsActivity : BaseActivity(), View.OnClickListener {
         val dataSign = HttpUtils.encrypt(requestData, API.AppKey, "UTF-8")
         val DataSign = HttpUtils.urlEncoder(dataSign, "UTF-8")
         val params = HashMap<String, String>()
-        params.put("RequestData",RequestData)
-        params.put("EBusinessID",  API.EBusinessID)
+        params.put("RequestData", RequestData)
+        params.put("EBusinessID", API.EBusinessID)
         params.put("RequestType", "1002")
         params.put("DataSign", DataSign)
         params.put("DataType", "2")
         BaseRetrofit.instance
                 .createReq(APIService::class.java)
                 .searchDetailsData(params)
-                .enqueue(object : CustemCallBack<RequestEbsDetail> (){
+                .enqueue(object : CustemCallBack<RequestEbsDetail>() {
                     override fun onSuccess(response: Response<RequestEbsDetail>?) {
                         if (response!!.body()!!.Success) {
                             adapter!!.update(response.body()!!.Traces!!)
